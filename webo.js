88b9ca497797d863cc1f607a66c9b91f409fc303
Camilo Arango <camilo.arango2006@gmail.com>
2021-04-22 10:23:03 -0500

    Luna Momento

--- webo.js
+++ webo.js
@@ -2,8 +2,9 @@ window.addEventListener("scroll", function () {
   let scrolll = window.scrollY;
   let value1 = 255 + (scrolll / 200) * -255;
   let value2 = (scrolll / 200) * 1;
+  let value3 = 1 + (scrolll / 200) * -1;
   // document.documentElement.style.setProperty("");
-  console.log(value1);
+  console.log(value3);
   document.documentElement.style.setProperty(
     "--nav-a-color",
     `rgba(${value1},${value1},${value1},1)`
@@ -12,4 +13,5 @@ window.addEventListener("scroll", function () {
     "--nav-bg-color",
     `rgba(255,255 ,255,${value2})`
   );
+  document.documentElement.style.setProperty("--nav-luna-opacity", `${value3}`);
 });
